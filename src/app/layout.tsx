8bc114344efd1a9c5ed284ccaea9519786192190
Antoine-Bottin <a.bottin.dev@gmail.com>
2026-05-27 14:08:21 +0200

    feat(Layout): meta data, speed insight and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans, Literata } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.scss";
 
 const PlusJakartaSansFont = Plus_Jakarta_Sans({
@@ -14,7 +16,28 @@ const LiterataFont = Literata({
 
 export const metadata: Metadata = {
   title: "Mairie de Sauqueville",
-  description: "Site web de la commune de Sauqueville",
+  description: "Site internet de la mairie de Sauqueville",
+  creator: "Antoine Bottin",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  keywords: [
+    "Mairie",
+    "Sauqueville",
+    "Mairie de Sauqueville",
+    "76550",
+    "Communauté d'agglomération de la ville de Dieppe",
+    "Evènements",
+    "Vie communale",
+    "Associations",
+    "Informations",
+  ],
+  metadataBase: new URL("https://www.sauqueville.fr"),
+  openGraph: {
+    title: "Mairie de Sauqueville ",
+    description: `Site internet de la mairie de Sauqueville`,
+    images: [{ url: "/assets/logo-mairie-black.svg" }],
+    type: "website",
+    url: "https://www.sauqueville.fr",
+  },
 };
 
 export default function RootLayout({
@@ -27,6 +50,8 @@ export default function RootLayout({
       lang="fr"
       className={`${PlusJakartaSansFont.variable} ${LiterataFont.variable}`}
     >
+      <SpeedInsights />
+      <Analytics />
       <body className={PlusJakartaSansFont.className}>{children}</body>
     </html>
   );
